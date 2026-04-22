4870450b5bf0c577ac557ea37c1b6e446c6f5615
miamayalykke <miamayalykke@gmail.com>
2026-04-22 10:22:21 +0200

    fix: await MyOrders promise

--- src/sanity/lib/orders/getMyOrders.tsx
+++ src/sanity/lib/orders/getMyOrders.tsx
@@ -1,7 +1,8 @@
 import { defineQuery } from 'next-sanity'
+import type { MY_ORDERS_QUERYResult } from '../../../../sanity.types'
 import { client } from '../client'
 
-export async function getMyOrders(userId: string) {
+export async function getMyOrders(userId: string): Promise<MY_ORDERS_QUERYResult> {
   if (!userId) {
     throw new Error('User ID is required')
   }
@@ -16,7 +17,7 @@ export async function getMyOrders(userId: string) {
     `)
 
   try {
-    const orders = await client.fetch(MY_ORDERS_QUERY, { userId })
+    const orders = await client.fetch<MY_ORDERS_QUERYResult>(MY_ORDERS_QUERY, { userId })
     return orders || []
   } catch (error) {
     console.error('Error fetching orders:', error)
