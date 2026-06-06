25fe9fad19b7498d43c8f72ed597ac5736265eec
miamayalykke <miamayalykke@gmail.com>
2026-06-06 12:24:11 +0200

    chore: add analytics and update nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '@src/components/headers/Header'
 import type { Metadata } from 'next'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const metadata: Metadata = {
   title: 'Intetkøn',
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className="bg-background">
         <Analytics />
+        <SpeedInsights />
         <ClerkProvider dynamic>
           <Header />
 
